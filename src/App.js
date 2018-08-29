2f48adff8db6db042585ec5d026dc89fe0ef0b69
zxc0328 <120797887@qq.com>
2018-08-29 16:47:59 +0800

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { createElement, Component } from "rax";
+import View from "rax-view";
+import Text from "rax-text";
+import Image from "rax-image";
+import ScrollView from "rax-scrollview";
+import styles from "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.app}>
+       
+        <ScrollView
+          style={{
+            height: screen.height
+          }}
+        >
+           <Image source={require("./assets/calendar.png")} />
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+export default App;
